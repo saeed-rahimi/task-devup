07a03754bd7f3f950b9f5e32a0fcde06c07062f4
saeed rahimi <saeedteachcodee@email.com>
2024-11-15 18:32:00 +0330

    fix

--- useoptianalchaninginnestedobjectc.js
+++ useoptianalchaninginnestedobjectc.js
@@ -1,21 +1,18 @@
 "use strict"
 function getUserContact(user) {
-   
-    return user.contact && user.contact.phone ? user.contact.phone : "Phone number not available";
-  }
-  
+  return user?.contact?.phone || "Phone number not available";
+}
 
-  const userWithPhone = {
-    name: "Ali",
-    contact: { phone: "123-456-7890" },
-  };
-  
-  const userWithoutPhone = {
-    name: "saeed"
-  };
-  
+const userWithPhone = {
+  name: "Ali",
+  contact: { phone: "123-456-7890" },
+};
 
-  console.log(getUserContact(userWithPhone));  
-  console.log(getUserContact(userWithoutPhone)); 
-   
+const userWithoutPhone = {
+  name: "saeed",
+};
+
+console.log(getUserContact(userWithPhone)); 
+console.log(getUserContact(userWithoutPhone)); 
+
   
